b8e4686fc99e6e0604ea50f28ddee9a2597b059e
Krzysztof Karczewski <kr.karczewski@gmail.com>
2020-11-20 12:06:50 +0100

    Initial commit

---
+++ androidautotestapp1/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+val kotlin_version by extra("1.4.10")
+android {
+    compileSdkVersion(29)
+
+    defaultConfig {
+        applicationId = "com.github.karczews.androidauto.automotive.testapp1"
+        minSdkVersion(28)
+        targetSdkVersion(29)
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core:1.5.0-alpha05")
+    implementation("com.google.android.libraries.car:car-app:1.0.0-beta.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlin_version")
+    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.appcompat:appcompat:1.2.0")
+}
---
+++ androidautotestapp1/src/main/java/com/github/karczews/androidauto/automotive/car/TestNavigationService.kt
@@ -0,0 +1,11 @@
+package com.github.karczews.androidauto.automotive.car
+
+import android.content.Intent
+import com.google.android.libraries.car.app.CarAppService
+import com.google.android.libraries.car.app.Screen
+
+class TestNavigationService : CarAppService() {
+    override fun onCreateScreen(intent: Intent): Screen {
+        TODO("Not yet implemented")
+    }
+}
